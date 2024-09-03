5b781f2ba4a2258d8854f50fd998527542b1ca2b
e-chan-belov <echanbelov@icloud.com>
2024-09-03 15:07:40 +0300

    Initial Commit

---
+++ Two-sided list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Two-sided list
+//
+//  Created by Белов Егор on 03.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
